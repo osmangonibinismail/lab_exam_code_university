216f5d0434d33e0b89ffe86fb203b849211c94ef
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-11-26 20:47:27 +0600

    problem solve

--- programming.c
+++ programming.c
@@ -61,39 +61,39 @@
 // // number H4 answer
 
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     int n, m, s;
-//     scanf("%d %d", &n, &m);
+int main() {
+    int n, m, s;
+    scanf("%d %d", &n, &m);
 
-//     scanf("%d", &s);
+    scanf("%d", &s);
 
-//     int array[n][m];
+    int array[n][m];
     
-//     for (int i = 0; i < n; i++) {
-//         for (int j = 0; j < m; j++) {
-//             scanf("%d", &array[i][j]);
-//         }
-//     }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf("%d", &array[i][j]);
+        }
+    }
 
-//     int count = 0;
+    int count = 0;
 
-//     for (int j = 0; j < m; j++) {
-//         int sum = 0;
-//         for (int i = 0; i < n; i++) {
-//             sum += array[i][j];
-//         }
-//         if (sum == s) {
-//             count++;
-//         }
-//     }
+    for (int j = 0; j < m; j++) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += array[i][j];
+        }
+        if (sum == s) {
+            count++;
+        }
+    }
 
     
-//     printf("%d\n", count);
+    printf("%d\n", count);
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 
